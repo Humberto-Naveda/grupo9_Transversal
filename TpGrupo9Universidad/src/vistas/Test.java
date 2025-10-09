44c819bfc851c0800003ed87931c3d1c1c9d93a4
saul roldan <saul28roldan@gmail.com>
2025-10-09 19:14:34 -0300

    clase materia

--- TpGrupo9Universidad/src/vistas/Test.java
+++ TpGrupo9Universidad/src/vistas/Test.java
@@ -13,13 +13,13 @@ public class Test {
         Conexion conn = new Conexion("grupo_9_universidad","jdbc:mariadb://localhost/","root","","org.mariadb.jdbc.Driver");
         AlumnoData alum = new AlumnoData(conn);
         
-        Alumno alumno1 = new Alumno(29400899,"Simpson","Bart",LocalDate.of(1980,2,23),true);
+        Alumno alumno3 = new Alumno(290089,"Simpson","at",LocalDate.of(1900,2,23),true);
         
-        alum.ingresarAlumno(alumno1);
+        alum.ingresarAlumno(alumno3);
         
-        alum.actualizarAlumno(alumno1);
+       
         
-        System.out.println(alumno1);
+        System.out.println(alumno3);
     }
     
 }
