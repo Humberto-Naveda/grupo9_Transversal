990bcd9cd31ad62af3b5c01aa78620d5418f72fc
Humberto Naveda <122648734+Humberto-Naveda@users.noreply.github.com>
2025-10-18 10:34:46 -0300

    Entrega  Final

--- archivos/grupo_9_universidad.sql
+++ archivos/grupo_9_universidad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2025 a las 04:26:37
+-- Tiempo de generación: 18-10-2025 a las 15:26:59
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
